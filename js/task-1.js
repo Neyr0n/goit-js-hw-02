980914a1f8b81fcc8ee1d41b3678ec46905927ec
Vladislav <neyron232@gmail.com>
2024-04-07 17:17:26 +0200

    solved all the tasks

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,14 @@
-function makeTransaction(quantity, pricePerDroid) {
-  return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  const sum = quantity * pricePerDroid;
+  if (customerCredits >= sum) {
+    return console.log(`You ordered ${quantity} droids worth ${sum} credits!`);
+  } else {
+    return console.log(`Insufficient funds!`);
+  }
 }
 
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+makeTransaction(5, 3000, 23000);
+makeTransaction(3, 1000, 15000);
+makeTransaction(10, 5000, 8000);
+makeTransaction(8, 2000, 10000);
+makeTransaction(10, 500, 5000);
